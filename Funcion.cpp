b2f418e3b7cef7a754fcc4782335b2a2bd542330
Gabriel López <gamalop0104@gmail.com>
2024-07-20 15:11:25 -0500

    1.1.0_MenuPrincipal

--- Funcion.cpp
+++ Funcion.cpp
@@ -1,16 +1,103 @@
 #define dim 100
 #include <iostream>
+#include <cstdlib>
+#include <conio.h>
+#include <windows.h>
 using namespace std;
+#define TECLA_ARRIBA 72
+#define TECLA_ABAJO 80
+#define ENTER 13
 
-void LlenarArreglo(int A[dim]);
+void LlenarArregloQuickSort(int A[dim]);
 void Quicksort( int A[dim], int primero, int ultimo);
-void mostrarArreglo(int A[dim]);
+void mostrarArreglo(int A[dim], int tamanio);
+void Menu_Principal();
+void gotoxy(int x, int y);
+int Menu(const char *titulo,const char *opciones[],int tamanio);
 int main(){
-
+    Menu_Principal();
     return 0;
 }
 
-void LlenarArregloQuickSoert(int A[dim]){
+void gotoxy(int x, int y) {
+    COORD coord;
+    coord.X = x;
+    coord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+
+int Menu(const char *titulo,const char *opciones[],int tamanio){
+    int opcionSelec=1, tecla;
+    bool seguir=true;
+    do{
+        system("cls");
+        gotoxy(5,3+ opcionSelec); cout<<">";
+
+        gotoxy(20,2); cout<<titulo;
+        for(int i=0;i<tamanio;i++){
+            gotoxy(10,4 + i); cout<<i+1<<") "<<opciones[i];
+        }
+        do{
+        tecla=getch();
+        }while(tecla!=TECLA_ARRIBA&&tecla!=TECLA_ABAJO&&tecla!=ENTER);
+
+        switch (tecla)
+        {
+        case TECLA_ARRIBA:
+            opcionSelec--;
+            if(opcionSelec<1){
+                opcionSelec=tamanio;
+            }
+            break;
+        case TECLA_ABAJO:
+            opcionSelec++;
+            if(opcionSelec>tamanio){
+                opcionSelec=1;
+            }
+            break;
+        case ENTER:
+            seguir=false;
+            break;
+        }
+    }while(seguir);
+    return opcionSelec;
+}
+void Menu_Principal(){
+    bool seguir=true;
+    
+    const char *titulo ="Menu Principal";
+    const char *opciones[]={"Permutacion","Combinacion","Recursion","Induccion","Ordenacion","Busqueda","Salir"};
+    int opcion,numop=7,A[dim];
+    do{
+        opcion=Menu(titulo, opciones,numop);
+        switch (opcion)
+        {
+        case 1:
+
+            break;
+        case 2:
+
+            break;
+        case 3:
+
+            break;
+        case 4:
+
+            break;
+        case 5:
+            LlenarArregloQuickSort(A);
+            break;
+        case 6:
+
+            break;
+        case 7:
+            seguir=false;
+            break;
+        
+        }
+    }while(seguir);
+}
+void LlenarArregloQuickSort(int A[dim]){
     int tamanio=-1;
 
     while(tamanio<0||tamanio>100){
@@ -72,7 +159,6 @@ void Quicksort( int A[dim], int primero, int ultimo){
 
 void mostrarArreglo(int A[dim], int tamanio)
 {
-
     for (int i = 0; i <= tamanio - 1; i++)
     {
         cout << A[i] <<" ";
